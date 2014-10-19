cb70778cec909029c46f45c1f7d0abf60098439e
Shashwat <shashwat@Chetans-MacBook-Air.local>
2014-10-18 23:19:52 -0700

    Friction, more complex model of truth.

--- sim.py
+++ sim.py
@@ -19,11 +19,61 @@ def sys(x, t):
                 ( M + m - m*pow(cos(x[0]),2) ))
     return np.asarray([x_0_dot, x_1_dot, x_2_dot, x_3_dot, 0])
 
+def dope_sys(state, t):
+    M = 0.5     # [kg]      mass of the cart
+    m = 0.2     # [kg]      mass of the pendulum
+    l = 0.3     # [m]       length from pendulum center to cart
+    I = 0.006   # [kg*m^2]  moment of inertia
+    g = -9.81   # [m/(s^2)] acceleration from gravity
+    b = 0.1     # [N/m/sec] coeffisient of friction of cart
+    a = 0.1     # [N/r/sec] coeffisient of friction of pendulum
+
+
+    theta       = state[0]
+    theta_dot   = state[1]
+    s_dot       = state[3]
+    u           = state[4]
+
+
+    m_l             = m*l
+    m_l2            = m_l*m_l  #m2_l2 for readability?
+    m_l3            = m_l2*m_l
+    M_m             = M + m
+    cos_theta       = cos(theta)
+    sin_theta       = sin(theta)
+    cos2_theta      = cos_theta*cos_theta
+    sin2_theta      = sin_theta*sin_theta
+    cossin_theta    = sin_theta*cos_theta
+    I_p             = I + m_l*l
+    theta_dot2      = theta_dot*theta_dot
+    m_l2_cos_theta2 = m_l2*cos2_theta  # name should be cos2_theta
+
+
+    s_ddot      = (( I_p * u 
+                   + I_p * m_l * theta_dot2 * sin_theta
+                   + g * m_l2 * cossin_theta
+                   - I_p * b * s_dot )
+                  / ( I_p * M_m
+                  + m_l2_cos_theta2 ))
+
+
+    theta_ddot  = ((( m_l * cos_theta * u 
+                    + m_l2 * cossin_theta * theta_dot2
+                    - m_l * cos_theta * b * s_dot 
+                    + ( g * m_l3 * cossin_theta * cos_theta / I_p ))
+                  / ( I_p * M_m + m_l2_cos_theta2 ))
+                - ( ( m_l * g * sin_theta + a * theta_dot ) 
+                  / I_p ))
+
+
+
+    return np.asarray([theta_dot, theta_ddot, s_dot, s_ddot, 0])
+
 def next_step(x, u, t):
     dt = 0.1
     t = np.linspace(t, t+dt, 2)
     # print x.append(u)
-    x_next = odeint(sys, np.append(x,u), t)
+    x_next = odeint(dope_sys, np.append(x,u), t)
     return x_next[1,0:4], t[1]
 
 def simulate(x0, U):
